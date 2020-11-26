f5c913d054e960ff44f2f7cec806e5cff6678185
haru221 <haru221@mocha.ocn.ne.jp>
2020-11-26 21:10:00 +0900

    htmlを表示するように修正

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 'use strict';
 const http = require('http');
+const fs = require('fs');
+const htmlData = fs.readFileSync('index.html', 'utf8');
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
-  res.write(req.headers['user-agent']);
-  res.end();
+  res.end(htmlData);
 });
 const port = 8000;
 server.listen(port, () => {
